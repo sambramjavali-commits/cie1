ee553898340028db9df6b85dccd7d36db4375fd5
sambram <sambramjavali@gmail.com>
2026-09-09 14:27:59 +0530

    cie 1

---
+++ cie 1.py
@@ -0,0 +1,5 @@
+initialbalance =int(input("enter the initial balance"))
+depositamount =int(input("enter the amount you want to deposit"))
+print("intial amount:",initialbalance)
+print("amount deposited",depositamount)
+print("total amount:",initialbalance+depositamount)
