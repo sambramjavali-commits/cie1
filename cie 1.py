0538c97d3018a2c8f160196b7d4018046ccad29e
sambramjavali-commits <sambramjavali@gmail.com>
2026-09-09 14:33:48 +0530

    Update cie 1.py

--- cie 1.py
+++ cie 1.py
@@ -1,5 +1,8 @@
 initialbalance =int(input("enter the initial balance"))
 depositamount =int(input("enter the amount you want to deposit"))
+withdrawamt = int(input("enter the amount you want to withdraw")
 print("intial amount:",initialbalance)
 print("amount deposited",depositamount)
 print("total amount:",initialbalance+depositamount)
+print("total amount:",initialbalance+depositamount-withdrawamt)                  
+                  
