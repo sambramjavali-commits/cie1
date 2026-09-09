d3f25bfedabaedb1b0d4106ad333f2b6763eeb63
sambramjavali-commits <sambramjavali@gmail.com>
2026-09-09 15:42:15 +0530

    Update cie 1.py

--- cie 1.py
+++ cie 1.py
@@ -5,5 +5,5 @@ print("intial amount:",initialbalance)
 print("amount deposited",depositamount)
 print("total amount:",initialbalance+depositamount)
 print("AFTER WITHDRAWAL THE TOTAL amount:",initialbalance+depositamount-withdrawamount)        
-print("the total amount after withdraw",nitialbalance+depositamount-withdrawamt)                  
+print("the total amount after withdraw",initialbalance+depositamount-withdrawamt)                  
                   
